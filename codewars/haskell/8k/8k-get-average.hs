25b5de0bda5619e9ccb7a06c0cc16bc7143b00de
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-12-01 13:48:14 +0100

    codewars moved into separate repository

--- codewars/haskell/8k/8k-get-average.hs
+++
@@ -1,7 +0,0 @@
--- https://www.codewars.com/kata/563e320cee5dddcf77000158
--- Get the average value from an array. Round down to the nearest integer.
-
-module Codewars.AverageCalculator where
-
-getAverage :: [Int] -> Int
-getAverage x = div (sum x) (length x)
