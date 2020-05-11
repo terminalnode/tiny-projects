0d258b609f902733566a31f26899050ff339230b
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-05-11 18:49:09 +0200

    angular ToH '2. Display a List'

---
+++ tutorials/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Nobunaga' },
+  { id: 12, name: 'Tōkichiro' },
+  { id: 13, name: 'Sakamoto' },
+  { id: 14, name: 'Oda' },
+  { id: 15, name: 'Hideyoshi' },
+  { id: 16, name: 'Toyotomi' },
+  { id: 17, name: 'Ryōma' },
+  { id: 18, name: 'Tokugawa' },
+  { id: 19, name: 'Ieyasu' },
+  { id: 20, name: 'Kuroda' },
+];
